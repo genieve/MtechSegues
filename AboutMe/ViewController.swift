fb36ff772b8cae2d5cd9075d4b07993585c11bda
Alexis <alexisgenieve@gmail.com>
2023-02-03 09:38:32 -0700

    Initial Commit

---
+++ AboutMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutMe
+//
+//  Created by Alexis Wright on 2/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
